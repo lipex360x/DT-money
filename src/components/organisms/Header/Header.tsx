f86699af5801ccc0a148f21de989da072d533546
Felipe Bambirra <lipex360@gmail.com>
2022-08-27 12:20:30 -0300

    Header updated

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,18 +1,35 @@
 import { Button } from '@/components/atoms/Button'
 import { Logo } from '@/components/atoms/Logo'
+import { Modal } from '@/components/molecules/Modal'
 import { Container } from '@/components/bosons/Container'
 
 import * as S from './styles'
+import { useState } from 'react'
+import { TransactionForm } from '@/components/molecules/TransactionForm'
 
 export const Header = () => {
+  const [modalState, setModalState] = useState(true)
+
+  const handleModalState = () => {
+    setModalState(!modalState)
+  }
+
   return (
     <S.Wrapper>
       <Container>
         <S.Content>
           <Logo />
-          <Button>Nova Transação</Button>
+          <Button onClick={handleModalState}>Nova Transação</Button>
         </S.Content>
       </Container>
+
+      <Modal
+        isOpen={modalState}
+        modalState={handleModalState}
+        title='Nova Transação'
+        content={<TransactionForm />}
+      />
+
     </S.Wrapper>
   )
 }
